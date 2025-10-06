f75ade6c3e4f4426fb4e4d13651210b8fc6da3a4
CS Rhymes <chrisrhymes@users.noreply.github.com>
2025-10-06 16:59:44 +0100

    Fix linter

--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Timer = () => {
   const [minutesRemaining, setMinutesRemaining] = useState<number>(1);
