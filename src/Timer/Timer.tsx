e28920a7d1006c82f8b84ddf4a9ecf179269013f
CS Rhymes <chrisrhymes@users.noreply.github.com>
2025-10-07 08:41:53 +0100

    Replace alert with modal and animate time when 5 seconds remaining

--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
 const Timer = () => {
-  const [minutesRemaining, setMinutesRemaining] = useState<number>(1);
-  const [secondsRemaining, setSecondsRemaining] = useState<number>(59);
+  const [minutesRemaining, setMinutesRemaining] = useState<number>(25);
+  const [secondsRemaining, setSecondsRemaining] = useState<number>(0);
   const [timerRunning, setTimerRunning] = useState<boolean>(false);
+  const [timerFinished, setTimerFinished] = useState<boolean>(false);
+  const [timerNear, setTimerNear] = useState<boolean>(false);
 
   useEffect(() => {
     if (timerRunning) {
@@ -15,7 +17,7 @@ const Timer = () => {
           if (minutesRemaining === 0) {
             clearInterval(intervalId);
             setTimerRunning(false);
-            alert("Time is up!");
+            setTimerFinished(true);
           } else {
             setMinutesRemaining((minutesRemaining) => minutesRemaining - 1);
             setSecondsRemaining(59);
@@ -26,6 +28,12 @@ const Timer = () => {
     }
   }, [timerRunning, minutesRemaining, secondsRemaining]);
 
+  useEffect(() => {
+    if (minutesRemaining === 0 && secondsRemaining <= 5) {
+      setTimerNear(true);
+    }
+  }, [minutesRemaining, secondsRemaining]);
+
   const handleTimerStart = () => {
     if (secondsRemaining <= 0 && minutesRemaining <= 0) {
       alert(
@@ -34,14 +42,50 @@ const Timer = () => {
       return;
     }
     setTimerRunning(true);
+    setTimerFinished(false);
+    setTimerNear(false);
   };
 
   const handleTimerStop = () => {
     setTimerRunning(false);
+    setTimerNear(false);
+  };
+
+  const closePopUp = () => {
+    setTimerFinished(false);
+    setMinutesRemaining(25);
+    setSecondsRemaining(0);
+    setTimerNear(false);
   };
 
   return (
-    <div className="flex w-full flex-col flex-wrap text-center lg:w-1/2">
+    <div className="relative flex w-full flex-col flex-wrap text-center lg:w-1/2">
+      {timerFinished && (
+        <span className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col rounded bg-sky-50 px-16 pt-16 pb-8 shadow-2xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-24 animate-bounce"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <p className="my-6">Time is up!</p>
+          <button
+            type="submit"
+            className="w-full cursor-pointer rounded bg-slate-800 px-4 py-2 text-white hover:bg-slate-900 focus:outline-4 focus:outline-amber-400"
+            onClick={closePopUp}
+          >
+            Close
+          </button>
+        </span>
+      )}
       {!timerRunning ? (
         <>
           <h1 className="mb-4 text-2xl font-bold">Set your timer!</h1>
@@ -56,7 +100,7 @@ const Timer = () => {
                 type="number"
                 name="minutes"
                 onChange={(e) => setMinutesRemaining(parseInt(e.target.value))}
-                defaultValue={minutesRemaining}
+                value={minutesRemaining}
                 className="w-full border-2 border-slate-800 bg-white px-4 py-2 focus:outline-4 focus:outline-amber-400 active:outline-4 active:outline-amber-400"
                 min={0}
                 max={59}
@@ -69,7 +113,7 @@ const Timer = () => {
                 type="number"
                 name="seconds"
                 onChange={(e) => setSecondsRemaining(parseInt(e.target.value))}
-                defaultValue={secondsRemaining}
+                value={secondsRemaining}
                 className="w-full border-2 border-slate-800 bg-white px-4 py-2 focus:outline-4 focus:outline-amber-400 active:outline-4 active:outline-amber-400"
                 max={59}
                 min={0}
@@ -87,7 +131,9 @@ const Timer = () => {
         </>
       ) : (
         <div className="flex flex-col items-center">
-          <p className="mb-8 font-mono text-8xl font-bold">
+          <p
+            className={`mb-8 font-mono text-8xl font-bold ${timerNear && "animate-ping text-red-800"}`}
+          >
             {minutesRemaining.toString().padStart(2, "0")}:
             {secondsRemaining.toString().padStart(2, "0")}
           </p>
